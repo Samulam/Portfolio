0182bff079fbce2840ad52e682f3839b4cfd35ac
Samulam <57199284+Samulam@users.noreply.github.com>
2020-04-16 14:57:02 -0700

    portfolio files from repl.it

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Sammy period 7</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Projects.html">Projects.html</a></li></br>
+      <li><a href="Resume.html">Resume.html</a></li></br>
+      <li><a href="ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
